c019db5db905b92c84ea17507e2bf557c9a62139
Samuel Pena <inestbht@gmail.com>
2022-06-12 21:06:54 -0500

    SAVED STATE - Ch. 6.2 - Toolbar and Locations List Table View - App: WeatherGift

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Samuel Pena on 6/12/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
