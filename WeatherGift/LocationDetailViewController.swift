c0be1786c6ce9334aeb7c42ec5a6189bd163062c
Samuel Pena <inestbht@gmail.com>
2022-06-13 21:17:23 -0500

    SAVED STATE - Ch. 6.6 - Beginning the Location Detail UI

---
+++ WeatherGift/LocationDetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LocationDetailViewController.swift
+//  WeatherGift
+//
+//  Created by Samuel Pena on 6/13/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var weatherLocation: WeatherLocation!
+    var weatherLocations: [WeatherLocation] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if weatherLocation == nil {
+            weatherLocation = WeatherLocation(name: "Current Location", latitude: 0.0, longitude: 0.0)
+            weatherLocations.append(weatherLocation)
+        }
+        
+        updateUserInterface()
+    }
+    
+    func updateUserInterface() {
+        dateLabel.text = ""
+        placeLabel.text = weatherLocation.name
+        temperatureLabel.text = "--°"
+        summaryLabel.text = ""
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! LocationListViewController
+        destination.weatherLocations = weatherLocations
+    }
+    
+    @IBAction func unwindFromLocationListViewController(segue: UIStoryboardSegue) {
+        let source = segue.source as! LocationListViewController
+        weatherLocations = source.weatherLocations
+        weatherLocation = weatherLocations[source.selectedLocationIndex]
+        updateUserInterface()
+    }
+
+}
--- WeatherGift/LocationListViewController.swift
+++ WeatherGift/LocationListViewController.swift
@@ -15,19 +15,24 @@ class LocationListViewController: UIViewController {
     @IBOutlet weak var addBarButton: UIBarButtonItem!
     
     var weatherLocations: [WeatherLocation] = []
+    var selectedLocationIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var weatherLocation = WeatherLocation(name: "Chestnut Hill, MA", latitude: 0, longitude: 0)
-        weatherLocations.append(weatherLocation)
-        weatherLocation = WeatherLocation(name: "Lilongwe, Malawi", latitude: 0, longitude: 0)
-        weatherLocations.append(weatherLocation)
-        weatherLocation = WeatherLocation(name: "Buenos Aires, Argentina", latitude: 0, longitude: 0)
-        weatherLocations.append(weatherLocation)
+//        var weatherLocation = WeatherLocation(name: "Chestnut Hill, MA", latitude: 0, longitude: 0)
+//        weatherLocations.append(weatherLocation)
+//        weatherLocation = WeatherLocation(name: "Lilongwe, Malawi", latitude: 0, longitude: 0)
+//        weatherLocations.append(weatherLocation)
+//        weatherLocation = WeatherLocation(name: "Buenos Aires, Argentina", latitude: 0, longitude: 0)
+//        weatherLocations.append(weatherLocation)
         
         tableView.dataSource = self
         tableView.delegate = self
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        selectedLocationIndex = tableView.indexPathForSelectedRow!.row
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
