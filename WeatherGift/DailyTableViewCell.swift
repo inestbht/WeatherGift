0333a6731f2b803644617c39865e764bfa97be69
Samuel Pena <inestbht@gmail.com>
2022-06-15 00:11:49 -0500

    SAVED STATE - Ch. 6.15 - Daily Weather in Custom TableView Cells - OpenWeather Update

---
+++ WeatherGift/DailyTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  DailyTableViewCell.swift
+//  WeatherGift
+//
+//  Created by Samuel Pena on 6/14/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import UIKit
+
+class DailyTableViewCell: UITableViewCell {
+    @IBOutlet weak var dailyImageView: UIImageView!
+    @IBOutlet weak var dailyWeekdayLabel: UILabel!
+    @IBOutlet weak var dailyHighLabel: UILabel!
+    @IBOutlet weak var dailySummaryView: UITextView!
+    @IBOutlet weak var dailyLowLabel: UILabel!
+    
+    var dailyWeather: DailyWeather! {
+        didSet {
+            dailyImageView.image = UIImage(named: dailyWeather.dailyIcon)
+            dailyWeekdayLabel.text = dailyWeather.dailyWeekday
+            dailySummaryView.text = dailyWeather.dailySummary
+            dailyHighLabel.text = "\(dailyWeather.dailyHigh)°"
+            dailyLowLabel.text = "\(dailyWeather.dailyLow)°"
+        }
+    }
+}
