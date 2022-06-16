626e0d3e6483c321083ac4acab011324f78dfb3f
Samuel Pena <inestbht@gmail.com>
2022-06-15 23:13:09 -0500

    COMPLETED - Ch. 6.22 - Finishing WeatherGift: LaunchScreen, About Screen, and App Icon

---
+++ WeatherGift/AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  WeatherGift
+//
+//  Created by Samuel Pena on 6/15/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
